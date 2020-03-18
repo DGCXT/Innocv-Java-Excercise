0c0cf838036fe8ba6abf5a6db1caa44ef142d63e
athena-git <athena.ai@protonmail.com>
2020-03-18 21:46:54 +0100

    Adds basic logging traces

--- src/main/java/com/david/innocv/controllers/UserController.java
+++ src/main/java/com/david/innocv/controllers/UserController.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.cfg.NotYetImplementedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,54 +20,62 @@ import org.springframework.web.bind.annotation.RestController;
 import com.david.innocv.exceptions.ResourceNotFoundException;
 import com.david.innocv.presentation.UserDTO;
 import com.david.innocv.services.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+	private static final Logger LOGGER=LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	UserService userService;
 
 	@GetMapping
 	public List<UserDTO> findAll()
 	{
+		LOGGER.info("Finding all users");
 		return userService.findAll();
 	}
 
 	@GetMapping("/{id}")
 	public UserDTO findById(@NotBlank @PathVariable("id") String id) throws ResourceNotFoundException
 	{
+		LOGGER.info("Finding user {}", id);
 		return userService.findById(id);
 	}
 
 	@GetMapping("/firstName/{firstName}")
 	public List<UserDTO> findByFirstName(@NotBlank @PathVariable("firstName") String firstName) throws ResourceNotFoundException
 	{
+		LOGGER.info("Finding users with first name {}", firstName);
 		return userService.findByFirstName(firstName);
 	}
 
 	@GetMapping("/lastName/{lastName}")
 	public List<UserDTO> findByLastName(@NotBlank @PathVariable("lastName") String lastName) throws ResourceNotFoundException
 	{
+		LOGGER.info("Finding users with last name {}", lastName);
 		return userService.findByLastName(lastName);
 	}
 
 	@PostMapping
 	public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO user)
 	{	
+		LOGGER.info("Creating user {}", user);
 		return new ResponseEntity<UserDTO>(userService.createUser(user), HttpStatus.CREATED);	
 	}
 
 	@PutMapping("/{id}")
 	public UserDTO updateUser(@NotBlank @PathVariable("id") String id, @Valid @RequestBody UserDTO user) throws ResourceNotFoundException
 	{	
+		LOGGER.info("Updating user {} with new values {}", id, user);
 		return userService.updateUser(id, user);
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<UserDTO> deleteUser(@PathVariable(value="id") String id) throws ResourceNotFoundException
 	{
+		LOGGER.info("Deleting user {}", id);
 		userService.deleteUser(id);
 		return new ResponseEntity<UserDTO>(HttpStatus.NO_CONTENT);
 	}
--- src/main/java/com/david/innocv/services/UserService.java
+++ src/main/java/com/david/innocv/services/UserService.java
@@ -5,9 +5,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.david.innocv.controllers.UserController;
 import com.david.innocv.entity.UserEntity;
 import com.david.innocv.exceptions.ResourceNotFoundException;
 import com.david.innocv.presentation.UserDTO;
@@ -16,6 +19,8 @@ import com.david.innocv.repositories.UserRepository;
 @Service
 public class UserService {
 
+	private static final Logger LOGGER=LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -23,6 +28,7 @@ public class UserService {
 	{
 		List<UserDTO> users = new ArrayList<UserDTO>();
 		userRepository.findAll().forEach(u -> users.add(UserDTO.fromUserEntity(u)));
+		LOGGER.info("Found users {}", users);
 		return users;
 	}
 	
@@ -31,6 +37,7 @@ public class UserService {
 		Optional<UserEntity> user = userRepository.findById(UUID.fromString(id));
 		if (!user.isPresent())
 			throw new ResourceNotFoundException(String.format("User with id %s not found.", id));
+		LOGGER.info("Found user {}", user.get());
 		return UserDTO.fromUserEntity(user.get());
 	}
 	
@@ -40,6 +47,7 @@ public class UserService {
 		userRepository.findByFirstName(firstName).forEach(u -> users.add(UserDTO.fromUserEntity(u)));
 		if (users.isEmpty())
 			throw new ResourceNotFoundException(String.format("No user with first name %s was found.", firstName));
+		LOGGER.info("Found user {}", users);
 		return users;
 	}
 	
@@ -49,12 +57,14 @@ public class UserService {
 		userRepository.findByLastName(lastName).forEach(u -> users.add(UserDTO.fromUserEntity(u)));
 		if (users.isEmpty())
 			throw new ResourceNotFoundException(String.format("No user with last name %s was found.", lastName));
+		LOGGER.info("Found user {}", users);
 		return users;
 	}
 	
 	public UserDTO createUser(UserDTO userDTO)
 	{
 		UserEntity userEntity = UserDTO.toUserEntity(userDTO);
+		LOGGER.info("Created user {}", userEntity);
 		return UserDTO.fromUserEntity(userRepository.save(userEntity));
 	}
 	
@@ -64,7 +74,9 @@ public class UserService {
 			throw new ResourceNotFoundException(String.format("User with id %S not found.", id));
 		UserEntity userEntity = UserDTO.toUserEntity(userDTO);
 		userEntity.setId(UUID.fromString(id));
-		return UserDTO.fromUserEntity(userRepository.save(userEntity));
+		userEntity = userRepository.save(userEntity);
+		LOGGER.info("Updated user {}", userEntity);
+		return UserDTO.fromUserEntity(userEntity);
 	}
 	
 	public void deleteUser(String id) throws ResourceNotFoundException
@@ -74,5 +86,6 @@ public class UserService {
 		UserEntity user = new UserEntity();
 		user.setId(UUID.fromString(id));
 		userRepository.delete(user);
+		LOGGER.info("Deleted user {}", user);
 	}
 }
