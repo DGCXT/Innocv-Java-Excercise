d268fb89dc95b191286a6c4123347ece3468c1f9
athena-git <athena.ai@protonmail.com>
2020-03-18 09:11:52 +0100

    Adds entry point class

---
+++ src/main/java/com/david/innocv/InnocvApplication.java
@@ -0,0 +1,12 @@
+package com.david.innocv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnocvApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(InnocvApplication.class, args);
+	}
+}
