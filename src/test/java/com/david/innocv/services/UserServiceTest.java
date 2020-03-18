434ac629d45982b6fa39e9a5d60c53270ab3b65b
athena-git <athena.ai@protonmail.com>
2020-03-18 21:50:14 +0100

    Changes spring annotations for Mockito annotations

--- src/test/java/com/david/innocv/services/UserServiceTest.java
+++ src/test/java/com/david/innocv/services/UserServiceTest.java
@@ -18,6 +18,10 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -27,14 +31,14 @@ import com.david.innocv.exceptions.ResourceNotFoundException;
 import com.david.innocv.presentation.UserDTO;
 import com.david.innocv.repositories.UserRepository;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 	
-	@Autowired
-	private UserService userService;
-	
-	@MockBean
+	@Mock
 	private UserRepository userRepository;
+
+	@InjectMocks
+	private UserService userService;
 	
 	private static UserDTO davidDTO;
 	
